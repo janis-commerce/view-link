73ea765898df55d0af3c6431d7c85224eb3feefb
Nataniel López <nataniel.lopez@fizzmod.com>
2019-10-07 17:27:30 -0300

    Inited with package-generator

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { ViewLink } = require('./lib');
+
+module.exports = ViewLink;
---
+++ tests/view-link-test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const assert = require('assert');
+
+const sandbox = require('sinon').createSandbox();
+
+const ViewLink = require('./../index');
+
+const ViewLinkError = require('./../lib/view-link-error')
+
+describe('ViewLink', () => {
+
+	// your tests here...
+
+});
